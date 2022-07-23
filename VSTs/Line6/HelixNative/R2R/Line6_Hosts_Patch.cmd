cc5b5bb77f024a4c18b0370fc829fa1c8a479cba
maxim <max20042011@gmail.com>
2022-07-23 23:43:03 +0300

    23.07.2022

---
+++ VSTs/Line6/HelixNative/R2R/Line6_Hosts_Patch.cmd
@@ -0,0 +1,46 @@
+@ECHO OFF
+
+ECHO @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ECHO @
+ECHO @ TEAM R2R PEACE MAKER v1.1.0
+ECHO @
+ECHO @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ECHO.
+ECHO Checking administrator right.
+
+OPENFILES >NUL 2>&1
+IF %ERRORLEVEL% EQU 0 (
+
+	ECHO Administrator right detected.
+	ECHO.
+	ECHO Hosts file : %WINDIR%\system32\drivers\etc\hosts
+	ECHO.
+
+	SETLOCAL ENABLEDELAYEDEXPANSION
+	
+		SET BLOCKLINE=0.0.0.0 line6.com
+		ECHO Checking : !BLOCKLINE!
+	    FIND /C /I "!BLOCKLINE!" "%WINDIR%\system32\drivers\etc\hosts" >NUL 2>NUL
+	    IF !ERRORLEVEL! NEQ 0 (
+	    	ECHO Line not found, adding to the hosts file.
+	    	ECHO !BLOCKLINE!>>%WINDIR%\system32\drivers\etc\hosts
+	    ) ELSE (
+	    	ECHO Line found.
+	    )
+    	ECHO.
+    	
+	ENDLOCAL
+	
+	ECHO Patching is completed.
+	ECHO Check hosts file if you want to see the result.
+	ECHO.
+
+) ELSE (
+
+	ECHO Administrator right not detected.
+    ECHO You need admin right to use this hosts patch!
+    ECHO.
+    
+)
+
+PAUSE
